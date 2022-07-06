ea306240dd26e35aba787a955da3bca9c73f91e4
unknown <jafarinarges20@gmail.com>
2022-07-06 16:38:25 +0430

    chore:fixed standard js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>hello
 
-      <HotelSearch/>
+      <HotelSearch />
     </div>
   )
 }
--- src/constants/search.js
+++ src/constants/search.js
@@ -1,13 +1,10 @@
 const stayDate = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5'
-  ]
-  
- 
-  export {
-stayDate
-  }
-  
+  '1',
+  '2',
+  '3',
+  '4',
+  '5'
+]
+export {
+  stayDate
+}
